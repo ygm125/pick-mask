3b79c5bc94e9cb6c1e8905ae08d893c5bb1bce1d
ygm125 <yuguangming125@126.com>
2015-08-08 23:43:00 +0800

    up

--- test/index.js
+++ test/index.js
@@ -1,8 +1,40 @@
 var pickMask = require('../lib');
 
-var t1 = {
+var obj = {
 	a : 1,
-	b : 2
+	b : 2,
+	c : {
+		a : 3,
+		b : 4,
+		c : 5
+	},
+	d : [
+		{
+			a : 5
+		},
+		{
+			b : 6
+		}	
+	],
+	ff : {
+		age : 18
+	},
+	dd : {
+		name : 'gm'
+	}
 }
 
-console.log(pickMask(t1,'a'));
+console.log(pickMask(obj,'a'));
+
+console.log(pickMask(obj,'a,c'));
+
+console.log(pickMask(obj,'c/a'));
+
+console.log(pickMask(obj,'c/(a,b)'));
+
+console.log(pickMask(obj,'c/*'));
+
+console.log(pickMask(obj,'#^\\w$'));
+
+
+
