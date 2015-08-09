be62e6011adba75535b70b5e99128c779a0f43f1
ygm125 <yuguangming125@126.com>
2015-08-09 11:47:02 +0800

    up

--- test/index.js
+++ test/index.js
@@ -27,17 +27,17 @@ var obj = {
 	}
 }
 
-console.log(pickMask(obj,'a'));
+console.log(pickMask(obj,'a')); // { a: 1 }
 
-console.log(pickMask(obj,'a,c'));
+console.log(pickMask(obj,'a,c')); // { a: 1 , c: { a: 3 , b: 4 , c: 5 } }
 
-console.log(pickMask(obj,'c.a'));
+console.log(pickMask(obj,'c.a')); // { c: { a: 3 } }
 
-console.log(pickMask(obj,'c@cc.(a,b)'));
+console.log(pickMask(obj,'c@cc.(a,b)')); // { cc: { a: 3, b: 4 } }
 
-console.log(pickMask(obj,'c.*'));
+console.log(pickMask(obj,'c.*')); //{ c: { a: 3, b: 4, c: 5 } }
 
-console.log(pickMask(obj,'/^\\w{2}$/'));
+console.log(pickMask(obj,'/^\\w{2}$/')); //{ ff: { age: 18} , dd:{ name: 'gm'} }
 
 
 
